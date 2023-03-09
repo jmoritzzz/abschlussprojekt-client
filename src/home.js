b8a8c95d74b04a7b9f8c37a465dfd0dabced51fa
jmoritzzz <moritz_jan@yahoo.de>
2023-03-09 09:05:18 +0100

    adding video

--- src/home.js
+++ src/home.js
@@ -1,15 +1,38 @@
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import background from './skyline_frasier.svg';
+import video from "./assets/frasier_intro_title.mp4";
 import Quotes from './components/Quotes';
 
 export default function Home() {
+
+    const [hover, setHover] = useState(false);
+
     return (
         <>
             <div className='home-container'>
                 <nav>
-                    <NavLink className="image-link" to="/Seasons">
+                    {/* <NavLink className="image-link" to="/Seasons">
                         <img className='skyline' src={background} alt="Skyline" />
+                    </NavLink> */}
+
+                    <NavLink className="image-link" to="/Seasons">
+                        <div className="title-wrapper">
+
+                            {hover ? (
+                                <video autoPlay src={video} onEnded={() => setHover(false)} />
+                            ) : (
+                                <img
+                                    className="skyline"
+                                    src={background}
+                                    alt="Skyline"
+                                    onMouseEnter={() => setHover(true)}
+                                    onMouseLeave={() => setHover(false)}
+                                />
+                            )}
+                        </div>
                     </NavLink>
+
                 </nav>
                 <div className='quote-container'>
 
