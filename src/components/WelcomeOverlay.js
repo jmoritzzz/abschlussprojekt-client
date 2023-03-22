a74e2115f130727991acba51e095a0e30d2fc232
jmoritzzz <115932741+jmoritzzz@users.noreply.github.com>
2023-03-22 18:33:13 +0100

    Update WelcomeOverlay.js

--- src/components/WelcomeOverlay.js
+++ src/components/WelcomeOverlay.js
@@ -17,7 +17,7 @@ function WelcomeOverlay() {
                 </div>
                 <hr className="hr2" />
                 <div className="welcome-overlay-text">
-                    <p>Ich habe in meiner Abschlußarbeit die Sitcom "Frasier" thematisiert. <br />
+                    <p>In meiner Abschlußarbeit habe ich die Sitcom "Frasier" thematisiert. <br />
                         Ich wollte eine Staffel- und Episodenübersicht, mit der Möglichkeit der Suche nach <br />
                         Gastdarstellenden, Gastanrufenden sowie speziellen Szenen oder Anmerkungen. <br />
                         Desweiteren habe ich mich bewußt für die deutsche Sprache entschieden. <br />
@@ -25,7 +25,7 @@ function WelcomeOverlay() {
                     <p>Die Umsetzung erfolgte mit HTML, CSS, React, MongoDB und Firebase, das Deploying bei Render. <br/> 
                     Leider sind die Ladezeiten etwas länger, was auch ein Grund ist, warum ich diesen Begrüßungstext verfasst habe. <br />
                         Die Seite ist kein abgeschlossenes Projekt. Es gibt noch einige Dinge, die ich umsetzen möchte. <br/>
-                        Das verwendeten urheberrechtlich geschütze Material dient zur Veranschaulichung im Rahmen des Unterrichts. </p>
+                        Das verwendete urheberrechtlich geschütze Material dient zur Veranschaulichung im Rahmen des Unterrichts. </p>
                     <p>Inzwischen sollte die Verbindung zur Datenbank stehen und ich wünsche viel Spaß!</p>
                 </div>
                 <button className="welcome-overlay-button" onClick={handleOverlayClick}>Weiter zur Seite</button>
