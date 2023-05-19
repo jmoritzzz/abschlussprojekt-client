9128a1a6f3408bb693b005956d23b2b00bc987bd
jmoritzzz <115932741+jmoritzzz@users.noreply.github.com>
2023-05-19 21:53:42 +0200

    Update WelcomeOverlay.js

--- src/components/WelcomeOverlay.js
+++ src/components/WelcomeOverlay.js
@@ -18,12 +18,12 @@ function WelcomeOverlay() {
                 <hr className="hr2" />
                 <div className="welcome-overlay-text">
                     <p>In meiner Abschlußarbeit habe ich die Sitcom "Frasier" thematisiert. <br />
-                        Ich wollte eine Staffel- und Episodenübersicht, mit der Möglichkeit der Suche nach <br />
+                        Ziel war eine Staffel- und Episodenübersicht, mit der Möglichkeit der Suche nach <br />
                         Gastdarstellenden, Gastanrufenden sowie speziellen Szenen oder Anmerkungen. <br />
-                        Desweiteren habe ich mich bewußt für die deutsche Sprache entschieden. <br />
+                        Ich habe mich bewußt für die deutsche Sprache entschieden. <br />
                         Es gibt in der Synchronisation einige Besonderheiten, die ich berücksichtigen möchte. </p>
-                    <p>Die Umsetzung erfolgte mit HTML, CSS, React, MongoDB und Firebase, das Deploying bei Render. <br/> 
-                    Leider sind die Ladezeiten etwas länger, was auch ein Grund ist, warum ich diesen Begrüßungstext verfasst habe. <br />
+                    <p>Die Umsetzung erfolgte mit HTML, CSS, React, Express, MongoDB und Firebase, das Deploying bei Render. <br/> 
+                    Leider sind die Ladezeiten etwas länger, was auch ein Grund für diesen Begrüßungstext ist. <br />
                         Die Seite ist kein abgeschlossenes Projekt. Es gibt noch einige Dinge, die ich umsetzen möchte. Das Design ist responsive, dennoch, der Thematik gezollt, nicht für die Betrachtung auf einem Smartphone ausgelegt. <br/>
                         Das verwendete urheberrechtlich geschütze Material dient zur Veranschaulichung im Rahmen des Unterrichts. </p>
                     <p>Inzwischen sollte die Verbindung zur Datenbank stehen und ich wünsche viel Spaß!</p>
